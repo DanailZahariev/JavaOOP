5e25dd8097b95f9e22bca1b6b3adf1396efe1403
Данаил Захариев <85960984+DanailZahariev@users.noreply.github.com>
2021-10-29 21:33:36 +0300

    update

--- src/WorkingWithAbstractionExercise/CardRank/Main.java
+++ src/WorkingWithAbstractionExercise/CardRank/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Card Ranks:");
         for (CardsRank value : CardsRank.values()) {
-            System.out.printf("Ordinal value: %d; Name value: %s%n", value.getValue(), value.name());
+            System.out.printf("Ordinal value: %d; Name value: %s%n", value.ordinal(), value.name());
         }
     }
 }
